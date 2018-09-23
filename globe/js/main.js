475624256a592983b0ef17e714acd9a2df643908
dimbo <mdimbo4@outlook.com>
2018-09-23 15:14:21 +0300

    update

--- globe/js/main.js
+++ globe/js/main.js
@@ -7,12 +7,17 @@ var WIDTH = window.innerWidth,
   windowHalfX = window.innerWidth / 2,
   windowHalfY = window.innerHeight / 2,
 
-  camera, scene, scene1, scene2, sceneall, mesh, group, renderer, controls, container, stats;
+  camera, scene, scene1, scene2, dots, sceneall, mesh, group, renderer, controls, container, stats;
+
+var raycaster;
+var mouse = new THREE.Vector2(),
+  INTERSECTED,
+  ids;
 
 var targetRotationX = 0;
 var targetRotationY = 0;
-var targetRotationOnMouseDownX = 0;
-var targetRotationOnMouseDownY = 0;
+var targetRotationXOnMouseDown = 0;
+var targetRotationYOnMouseDown = 0;
 var mouseX = 0;
 var mouseXOnMouseDown = 0;
 var mouseY = 0;
@@ -31,11 +36,42 @@ function init() {
   scene1 = new THREE.Scene();
   scene2 = new THREE.Scene();
   sceneall = new THREE.Scene();
+  dots = new THREE.Scene();
 
   camera = new THREE.PerspectiveCamera(45, WIDTH / HEIGHT, 1, 1000);
   camera.position.z = 1.5;
 
   scene.add(camera);
+
+  // add manual point ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+  var lat = [48.859175, 40.716599]
+  var lon = [2.349131, -73.995440]
+  var city = ["paris", "ny"]
+  ids = [];
+
+  for (var i = 0; i < lat.length; i++) {
+    var phi = (90 - lat[i]) * (Math.PI / 180),
+      theta = (lon[i] + 180) * (Math.PI / 180),
+      x1 = -((0.41) * Math.sin(phi) * Math.cos(theta)),
+      z1 = ((0.41) * Math.sin(phi) * Math.sin(theta)),
+      y1 = ((0.41) * Math.cos(phi));
+
+    var geometry = new THREE.SphereBufferGeometry(0.01, 32, 32);
+    var material = new THREE.MeshBasicMaterial({
+      color: 0xffffff
+    });
+    var mesh = new THREE.Mesh(geometry, material);
+    mesh.position.x = x1;
+    mesh.position.y = y1;
+    mesh.position.z = z1;
+    dots.add(mesh);
+    ids.push(city[i]);
+  }
+
+  // add manual point ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+  raycaster = new THREE.Raycaster();
 
   renderer = new THREE.WebGLRenderer({
     alpha: true
@@ -43,6 +79,8 @@ function init() {
   renderer.setPixelRatio(window.devicePixelRatio);
   renderer.setSize(WIDTH, HEIGHT);
   container.appendChild(renderer.domElement);
+
+  document.addEventListener('mousemove', onMouseMove, false);
 }
 
 scene.add(new THREE.AmbientLight(0x333333));
@@ -72,25 +110,7 @@ var mat2 = new THREE.MeshPhongMaterial({
 var clouds = new THREE.Mesh(geo2, mat2);
 scene2.add(clouds);
 
-// add manual point ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-var lat = [48.859175]
-var lon = [2.349131]
-
-var phi   = (90-lat[0])*(Math.PI/180),
-theta = (lon[0]+180)*(Math.PI/180),
-x1 = -((0.41) * Math.sin(phi)*Math.cos(theta)),
-z1 = ((0.41) * Math.sin(phi)*Math.sin(theta)),
-y1 = ((0.41) * Math.cos(phi));
-
-var dotGeometry = new THREE.Geometry();
-dotGeometry.vertices.push(new THREE.Vector3( x1, y1, z1));
-var dotMaterial = new THREE.PointsMaterial( { size: 15, sizeAttenuation: false } );
-var dot = new THREE.Points( dotGeometry, dotMaterial );
-scene1.add( dot );
-
-// add manual point ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
+scene1.add(dots);
 sceneall.add(scene1);
 sceneall.add(scene2);
 scene.add(sceneall);
@@ -125,6 +145,15 @@ function onDocumentMouseMove(event) {
   mouseY = event.clientY - windowHalfY;
   targetRotationX = targetRotationXOnMouseDown + (mouseX - mouseXOnMouseDown) * 0.02;
   targetRotationY = targetRotationYOnMouseDown + (mouseY - mouseYOnMouseDown) * 0.02;
+  mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
+  mouse.y = -(event.clientY / window.innerHeight) * 2 + 1;
+}
+
+function onMouseMove(event) {
+  event.preventDefault();
+
+  mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
+  mouse.y = -(event.clientY / window.innerHeight) * 2 + 1;
 }
 
 function onDocumentMouseUp(event) {
@@ -161,6 +190,17 @@ function onDocumentTouchMove(event) {
 
 render();
 
+function onHoverMouseDown() {
+  var city = ids[id - 9]
+  setTimeout(function () {
+    $('#' + city).addClass('active');
+    document.removeEventListener('mousedown', onDocumentMouseDown, false);
+    document.removeEventListener('touchstart', onDocumentTouchStart, false);
+    document.removeEventListener('touchmove', onDocumentTouchMove, false);
+    window.removeEventListener('resize', onWindowResize, false);
+  }, 100)
+}
+
 function animate() {
   scene1.rotation.y += 0.0005;
   scene2.rotation.y += 0.0008;
@@ -169,7 +209,40 @@ function animate() {
 }
 
 function render() {
+  raycaster.setFromCamera(mouse, camera);
+  var intersects = raycaster.intersectObjects(dots.children);
+  
+  if (intersects.length > 0) {
+    if (INTERSECTED != intersects[0].object) {
+        $('html,body').css('cursor', 'pointer');
+      if (INTERSECTED) INTERSECTED.material.color.setHex(INTERSECTED.currentHex);
+      INTERSECTED = intersects[0].object;
+      INTERSECTED.currentHex = INTERSECTED.material.color.getHex();
+      INTERSECTED.material.color.setHex(0xcccccc);
+      id = INTERSECTED.id;
+      document.addEventListener('click', onHoverMouseDown, false);
+    }
+  } else {
+        $('html,body').css('cursor', 'default');
+    if (INTERSECTED) INTERSECTED.material.color.setHex(INTERSECTED.currentHex);
+    INTERSECTED = null;
+    document.removeEventListener('click', onHoverMouseDown, false);
+  }
+  
+  renderer.render(scene, camera);
+
+
+
   sceneall.rotation.y = sceneall.rotation.y += (targetRotationX - sceneall.rotation.y) * 0.0125;
   sceneall.rotation.x = sceneall.rotation.x += (targetRotationY - sceneall.rotation.x) * 0.0125;
   renderer.render(scene, camera);
 }
+
+$(document).on('click', '#globe-container', function () {
+  if ($('.active').length > 0)
+    $('.open-box').removeClass('active')
+  document.addEventListener('mousedown', onDocumentMouseDown, false);
+  document.addEventListener('touchstart', onDocumentTouchStart, false);
+  document.addEventListener('touchmove', onDocumentTouchMove, false);
+  window.addEventListener('resize', onWindowResize, false);
+})
