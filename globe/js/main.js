d0290b9956deb31ec94dfc0be06c536892442fed
dimbo <mdimbo4@outlook.com>
2018-09-27 11:04:27 +0300

    update

--- globe/js/main.js
+++ globe/js/main.js
@@ -40,8 +40,7 @@ function init() {
   dots = new THREE.Scene();
 
   camera = new THREE.PerspectiveCamera(45, WIDTH / HEIGHT, 1, 1000);
-  
-  camera.position.z = 1.25;
+  camera.position.z = 1.5;
 
   scene.add(camera);
 
