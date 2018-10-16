56769169f2fc7e6877cfdc7bb2c0c16256deefc2
dimbo <mdimbo4@outlook.com>
2018-10-16 22:36:40 +0300

    react update

--- react/development/js/index.js
+++ react/development/js/index.js
@@ -9,73 +9,39 @@ import {
   createHashHistory
 } from 'history';
 
-var name;
-
-
-
-class NameForm extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {value: ''};
-
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-  }
-
-  handleChange(event) {
-    this.setState({value: event.target.value});
-  }
-
-  handleSubmit(event) {
-    if (this.state.value !== '') {
-      name = this.state.value;
-    } else {
-      name = 'Stranger';
-    }
+const skills = ['Creativity', 'Design', 'Development'];
+var i = 0;
     
-    function getName(name) {
-      return <span className="greeting"> {name}</span>
-    }
-    
-    const element = (
-      <div className="container">
-        <div className="row">
-          <div className="col-md-8 offset-md-2">
-            <h1>
-              Hello, {getName(name)}!
-            </h1>
-          </div>
-        </div>
-      </div>
-    )
-    
-    ReactDOM.render(
-      element,
-      document.getElementById('root')
-    );
-    event.preventDefault();
-  }
-
-  render() {
-    return (
-      <div className="container">
-        <div className="row">
-          <div className="col-md-8 offset-md-2">
-            <form onSubmit={this.handleSubmit}>
-              <label>
-                Name:
-                <input type="text" value={this.state.value} onChange={this.handleChange} />
-              </label>
-              <input type="submit" value="Submit" />
-            </form>
-          </div>
-        </div>
-      </div>
-    );
+function tick() {
+  const element = (
+    <div>
+      <h1>I propose a combination of multiple expertise: <span className="skillName">{skills[i]}</span></h1>
+    </div>
+  );
+  ReactDOM.render(
+    element,
+    document.getElementById('root')
+  );
+  var tl = new TimelineMax();
+  tl.fromTo($('.skillName'), .5, {
+    ease: Power3.easeOut,
+    opacity: 0,
+    top: 20
+  }, {
+    ease: Power3.easeOut,
+    opacity: 1,
+    top: 0
+  }, 0)
+  tl.to($('.skillName'), .5, {
+    ease: Power3.easeOut,
+    opacity: 0,
+    top: -20
+  }, 1)
+  if (i < (skills.length - 1)) {
+    i = i + 1
+  } else {
+    i = 0
   }
 }
 
-ReactDOM.render(
-  <NameForm />,
-  document.getElementById('root')
-);
+setInterval(tick, 2000);
