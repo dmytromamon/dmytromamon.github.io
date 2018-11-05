f58d314e5947ea10b80dac887a766b0b19c8db10
dimbo <mdimbo4@outlook.com>
2018-11-05 09:37:01 +0200

    update blog

--- website/development/js/index.js
+++ website/development/js/index.js
@@ -1,9 +1,48 @@
-"use strict";
-
-$(document).ready(function(){
-  var pageId = $('.wrapper').attr('data-page');
+// Hide Header on on scroll down
+jQuery(document).ready(function($) {
   
-  if (pageId == 'home') {
-    $('#pageId').html('You are at ' + pageId + ' page!');
+  if ($(window).width() < 992) {
+    var didScroll;
+    var lastScrollTop = 0;
+    var delta = 5;
+    var navbarHeight = $('.sidebar').outerHeight();
+
+    $(window).scroll(function(event){
+        didScroll = true;
+    });
+
+    setInterval(function() {
+        if (didScroll) {
+            hasScrolled();
+            didScroll = false;
+        }
+    }, 250);
+
+    function hasScrolled() {
+        var st = $(this).scrollTop();
+
+        // Make sure they scroll more than delta
+        if(Math.abs(lastScrollTop - st) <= delta)
+            return;
+
+        // If they scrolled down and are past the navbar, add class .nav-up.
+        // This is necessary so you never see what is "behind" the navbar.
+        if (st > lastScrollTop && st > navbarHeight){
+            // Scroll Down
+            $('.sidebar').addClass('sidebar-up');
+        } else {
+            // Scroll Up
+            if(st + $(window).height() < $(document).height()) {
+                $('.sidebar').removeClass('sidebar-up');
+            }
+        }
+
+        lastScrollTop = st;
+    }
   }
-})
+  
+  $('.strong-title').viewportChecker({
+      classToAdd: 'active',
+      repeat: true
+  });
+});
