2b2cbe8f7c7bb40f8b95802cc8865b03b3748105
dimbo <mdimbo4@outlook.com>
2018-11-10 00:12:27 +0200

    update

--- juan/development/js/index.js
+++ juan/development/js/index.js
@@ -1,4 +1,17 @@
 $(document).ready(function () {
+  
+  setTimeout(function(){
+    var tl = new TimelineMax();
+    
+    tl.staggerFrom($('#brand__svg *'), 2, {
+      ease: Power3.easeOut,
+      x: -300,
+      y: -150,
+      opacity: 0,
+      scale: 0
+    }, 0.01)
+  }, 1000)
+  
   var a = Math.floor(Math.random() * 6) + 4;
 
   for (i = 0; i < a; i++) {
@@ -23,84 +36,84 @@ $(document).ready(function () {
     ease: Power3.easeOut,
     opacity: 0
   }, 3)
-  
-  var count = $(window).width()*$(window).height()/16000;
-  
-  var blue = Math.floor(Math.random() * count) + count/4;
+
+  var count = $(window).width() * $(window).height() / 16000;
+
+  var blue = Math.floor(Math.random() * count) + count / 4;
 
   setTimeout(function () {
     for (i = 0; i < blue; i++) {
       var randomW = Math.floor(Math.random() * $(window).width()) + 1;
       var randomH = Math.floor(Math.random() * $(window).height()) + 1;
       var randomS = Math.floor(Math.random() * 75) + 10;
-      
+
       z = Math.floor(Math.random() * 10) + 1;
-      
+
       $('.wrapper').append('<div class="dot explosion-dot" style="transform: matrix(1, 0, 0, 1, ' + randomW + ', ' + randomH + '); width: ' + randomS + 'px; height: ' + randomS + 'px; background: #52b9d8; z-index: ' + z + '"></div>');
     };
 
-    var red = Math.floor(Math.random() * count/2) + count/8;
+    var red = Math.floor(Math.random() * count / 2) + count / 8;
 
     for (i = 0; i < red; i++) {
       var randomW = Math.floor(Math.random() * $(window).width()) + 1;
       var randomH = Math.floor(Math.random() * $(window).height()) + 1;
       var randomS = Math.floor(Math.random() * 75) + 10;
-      
+
       z = Math.floor(Math.random() * 10) + 1;
-      
+
       $('.wrapper').append('<div class="dot explosion-dot" style="transform: matrix(1, 0, 0, 1, ' + randomW + ', ' + randomH + '); width: ' + randomS + 'px; height: ' + randomS + 'px; background: #cb2b30; z-index: ' + z + '"></div>');
     };
 
-    var green = Math.floor(Math.random() * count/2) + count/8;
+    var green = Math.floor(Math.random() * count / 2) + count / 8;
 
     for (i = 0; i < green; i++) {
       var randomW = Math.floor(Math.random() * $(window).width()) + 1;
       var randomH = Math.floor(Math.random() * $(window).height()) + 1;
       var randomS = Math.floor(Math.random() * 75) + 10;
-      
+
       z = Math.floor(Math.random() * 10) + 1;
-      
+
       $('.wrapper').append('<div class="dot explosion-dot" style="transform: matrix(1, 0, 0, 1, ' + randomW + ', ' + randomH + '); width: ' + randomS + 'px; height: ' + randomS + 'px; background: #1c6f64; z-index: ' + z + '"></div>');
     };
 
-    var violet = Math.floor(Math.random() * count/2) + count/8;
+    var violet = Math.floor(Math.random() * count / 2) + count / 8;
 
     for (i = 0; i < violet; i++) {
       var randomW = Math.floor(Math.random() * $(window).width()) + 1;
       var randomH = Math.floor(Math.random() * $(window).height()) + 1;
       var randomS = Math.floor(Math.random() * 75) + 10;
-      
+
       z = Math.floor(Math.random() * 10) + 1;
-      
+
       $('.wrapper').append('<div class="dot explosion-dot" style="transform: matrix(1, 0, 0, 1, ' + randomW + ', ' + randomH + '); width: ' + randomS + 'px; height: ' + randomS + 'px; background: #57355d; z-index: ' + z + '"></div>');
     };
 
-    var orange = Math.floor(Math.random() * count/2) + count/8;
+    var orange = Math.floor(Math.random() * count / 2) + count / 8;
 
     for (i = 0; i < orange; i++) {
       var randomW = Math.floor(Math.random() * $(window).width()) + 1;
       var randomH = Math.floor(Math.random() * $(window).height()) + 1;
       var randomS = Math.floor(Math.random() * 75) + 10;
-      
+
       z = Math.floor(Math.random() * 10) + 1;
-      
+
       $('.wrapper').append('<div class="dot explosion-dot" style="transform: matrix(1, 0, 0, 1, ' + randomW + ', ' + randomH + '); width: ' + randomS + 'px; height: ' + randomS + 'px; background: #f3ae66; z-index: ' + z + '"></div>');
     };
 
-    var yellow = Math.floor(Math.random() * count/2) + count/8;
+    var yellow = Math.floor(Math.random() * count / 2) + count / 8;
 
     for (i = 0; i < yellow; i++) {
       var randomW = Math.floor(Math.random() * $(window).width()) + 1;
       var randomH = Math.floor(Math.random() * $(window).height()) + 1;
       var randomS = Math.floor(Math.random() * 75) + 10;
-      
+
       z = Math.floor(Math.random() * 10) + 1;
-      
+
       $('.wrapper').append('<div class="dot explosion-dot" style="transform: matrix(1, 0, 0, 1, ' + randomW + ', ' + randomH + '); width: ' + randomS + 'px; height: ' + randomS + 'px; background: #ffe552; z-index: ' + z + '"></div>');
     };
-    
+
   }, 3000);
-  
+
 });
 
 function moveDots(w, h, i) {
@@ -118,70 +131,70 @@ function moveDots(w, h, i) {
 }
 
 function roundDots(w, h, i) {
-  var wHalf = $(window).width()/2;
-  var hHalf = $(window).height()/2;
-  
+  var wHalf = $(window).width() / 2;
+  var hHalf = $(window).height() / 2;
+
   var tl = new TimelineMax();
-  
+
   if (w < wHalf) {
     if (h < hHalf) {
       // 1
       var dif = Math.random() * .75 + 0;
-      var newW = (Math.random() * wHalf + 1) - wHalf*dif
-      var newH = (Math.random() * hHalf + 1) - hHalf*(.75 - dif)
-      
+      var newW = (Math.random() * wHalf + 1) - wHalf * dif
+      var newH = (Math.random() * hHalf + 1) - hHalf * (.75 - dif)
+
       tl.to($('.dot').eq(i), 1, {
         ease: Power3.easeOut,
         x: newW,
         y: newH
       }, 0)
-      
+
       moveDots(newW, newH, i)
     } else {
       // 2
       var dif = Math.random() * .75 + 0;
-      var newW = (Math.random() * wHalf + 1) - wHalf*dif
-      var newH = (Math.random() * hHalf + hHalf) + hHalf*(.75 - dif)
-      
+      var newW = (Math.random() * wHalf + 1) - wHalf * dif
+      var newH = (Math.random() * hHalf + hHalf) + hHalf * (.75 - dif)
+
       tl.to($('.dot').eq(i), 1, {
         ease: Power3.easeOut,
         x: newW,
         y: newH
       }, 0)
-      
+
       moveDots(newW, newH, i)
     }
   } else {
     if (h < hHalf) {
       // 3
       var dif = Math.random() * .75 + 0;
-      var newW = (Math.random() * wHalf + wHalf) + wHalf*dif
-      var newH = (Math.random() * hHalf + 1) - hHalf*(.75 - dif)
-      
+      var newW = (Math.random() * wHalf + wHalf) + wHalf * dif
+      var newH = (Math.random() * hHalf + 1) - hHalf * (.75 - dif)
+
       tl.to($('.dot').eq(i), 1, {
         ease: Power3.easeOut,
         x: newW,
         y: newH
       }, 0)
-      
+
       moveDots(newW, newH, i)
     } else {
       // 4
       var dif = Math.random() * .75 + 0;
-      var newW = (Math.random() * wHalf + wHalf) + wHalf*dif
-      var newH = (Math.random() * hHalf + hHalf) + hHalf*(.75 - dif)
-      
+      var newW = (Math.random() * wHalf + wHalf) + wHalf * dif
+      var newH = (Math.random() * hHalf + hHalf) + hHalf * (.75 - dif)
+
       tl.to($('.dot').eq(i), 1, {
         ease: Power3.easeOut,
         x: newW,
         y: newH
       }, 0)
-      
+
       if (($('.dot').eq(i).position().left < 50) || ($('.dot').eq(i).position().top < 50) || ($('.dot').eq(i).position().left > ($(window).width() + 50)) || ($('.dot').eq(i).position().top < ($(window).height() + 50))) {
         $('.dot').eq(i).remove;
         console.log(i)
       }
-      
+
       moveDots(newW, newH, i)
     }
   }
@@ -192,48 +205,48 @@ $(document).on('click', '.click-dot', function () {
   var tl = new TimelineMax();
 
   tl.from($('.explosion-dot'), 1, {
-    ease: Power3.easeIn,
-    x: $(window).width() / 2 - 40,
-    y: $(window).height() / 2 - 40
-  }, 0)
-  .to($('.explosion-dot'), 1, {
-    ease: Power4.easeOut,
-    opacity: 1
-  }, 0)
-  .to($('.simple-dot'), 1, {
-    ease: Power4.easeOut,
-    opacity: 0
-  }, 0)
-  .to($('.click-dot'), 1, {
-    ease: Power4.easeOut,
-    opacity: 0
-  }, 0)
-  
-  setTimeout(function(){
+      ease: Power3.easeIn,
+      x: $(window).width() / 2 - 40,
+      y: $(window).height() / 2 - 40
+    }, 0)
+    .to($('.explosion-dot'), 1, {
+      ease: Power4.easeOut,
+      opacity: 1
+    }, 0)
+    .to($('.simple-dot'), 1, {
+      ease: Power4.easeOut,
+      opacity: 0
+    }, 0)
+    .to($('.click-dot'), 1, {
+      ease: Power4.easeOut,
+      opacity: 0
+    }, 0)
+
+  setTimeout(function () {
     $('.simple-dot').remove;
     $('.click-dot').remove;
   }, 1000)
-  
-  setTimeout(function(){
+
+  setTimeout(function () {
     for (i = 0; i < $('.dot').length; i++) {
       var currentW = $('.dot').eq(i).position().left;
       var currentH = $('.dot').eq(i).position().top;
-      
+
       roundDots(currentW, currentH, i);
     };
-  }, 1000)
+  }, 1250)
 })
 
-$(document).on('mousemove', '.explosion-dot', function(){
+$(document).on('mousemove', '.explosion-dot', function () {
   var i = $('.explosion-dot').index(this);
-  var wHalf = $(window).width()/2;
-  var hHalf = $(window).height()/2;
-  
+  var wHalf = $(window).width() / 2;
+  var hHalf = $(window).height() / 2;
+
   var tl = new TimelineMax();
-  
+
   var w = $('.explosion-dot').eq(i).position().left;
   var h = $('.explosion-dot').eq(i).position().top;
-  
+
   if (w < wHalf) {
     if (h < hHalf) {
       tl.to($('.explosion-dot').eq(i), .5, {
