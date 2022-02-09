194bd1f8da063ae55c850c2032ce455354ba70e4
Dmytro Mamon <mdimbo4@outlook.com>
2022-02-09 20:04:53 +0200

    re

--- react-app/precache-manifest.b9d3a40140b8a84591a96345ed6f8e2c.js
+++ react-app/precache-manifest.b9d3a40140b8a84591a96345ed6f8e2c.js
@@ -1,26 +1,26 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
     "revision": "a8cd805b8b240cb67d022dec5f32eb86",
-    "url": "/index.html"
+    "url": "/react-app/index.html"
   },
   {
     "revision": "aa01f9607deb754968b3",
-    "url": "/static/css/main.1215926c.chunk.css"
+    "url": "/react-app/static/css/main.1215926c.chunk.css"
   },
   {
     "revision": "b84ab64d09bd9e0b38fa",
-    "url": "/static/js/2.cdc6c22a.chunk.js"
+    "url": "/react-app/static/js/2.cdc6c22a.chunk.js"
   },
   {
     "revision": "c64c486544348f10a6d6c716950bc223",
-    "url": "/static/js/2.cdc6c22a.chunk.js.LICENSE.txt"
+    "url": "/react-app/static/js/2.cdc6c22a.chunk.js.LICENSE.txt"
   },
   {
     "revision": "aa01f9607deb754968b3",
-    "url": "/static/js/main.bf9649a9.chunk.js"
+    "url": "/react-app/static/js/main.bf9649a9.chunk.js"
   },
   {
     "revision": "8632f006499e46bf6856",
-    "url": "/static/js/runtime-main.f568361f.js"
+    "url": "/react-app/static/js/runtime-main.f568361f.js"
   }
 ]);
